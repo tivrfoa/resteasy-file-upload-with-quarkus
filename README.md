ea55834a4bd70b9fc713a431032bf7eeba806865
Leandro <lescoutinhovr@gmail.com>
2021-02-04 10:36:20 -0300

    File upload example in RESTEasy and Quarkus

---
+++ README.md
@@ -0,0 +1,28 @@
+I tried to find a Quarkus upload example, so I could share files in my local network.
+
+I found the: Quarkus guide: https://quarkus.io/guides/rest-client-multipart
+
+but it only shows how to produce a file, but not how to consume it.
+
+Then I found a bit old tutorial below from **2012**!!!
+https://mkyong.com/webservices/jax-rs/file-upload-example-in-resteasy/
+```
+File upload example in RESTEasy
+By mkyong | Last updated: August 29, 2012
+```
+
+I decided to give it a try, and was great surprise when it simply worked!!!
+
+That's the power of APIs and Java backwards compatibility!
+
+Great job by Quarkus on using these APIs!
+
+
+## Running the code
+
+`mvn quarkus:dev -Ddebug=false -Dquarkus.http.host=your-ip`
+
+Then go to:
+
+http://your-ip:8080/upload.html
+
---
+++ src/main/java/org/acme/rest/client/multipart/MultipartClientResource.java
@@ -0,0 +1,122 @@
+package org.acme.rest.client.multipart;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
+
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+
+import org.apache.commons.io.IOUtils;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+import org.jboss.resteasy.plugins.providers.multipart.InputPart;
+import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
+
+@Path("/client")
+public class MultipartClientResource {
+
+    @Inject
+    @RestClient
+    MultipartService service;
+
+    @POST
+    @Path("/multipart")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String sendFile() throws Exception {
+        MultipartBody body = new MultipartBody();
+        body.fileName = "greeting.txt";
+        body.file = new ByteArrayInputStream("HELLO WORLD".getBytes(StandardCharsets.UTF_8));
+        return service.sendMultipartData(body);
+    }
+
+    // https://mkyong.com/webservices/jax-rs/file-upload-example-in-resteasy/
+    private final String UPLOADED_FILE_PATH = ""; // will go to 'target' folder
+
+    @POST
+    @Path("/upload")
+    @Consumes("multipart/form-data")
+    public Response uploadFile(MultipartFormDataInput input) {
+
+        String fileName = "";
+
+        Map<String, List<InputPart>> uploadForm = input.getFormDataMap();
+        List<InputPart> inputParts = uploadForm.get("uploadedFile");
+
+        for (InputPart inputPart : inputParts) {
+
+            try {
+
+                MultivaluedMap<String, String> header = inputPart.getHeaders();
+                fileName = getFileName(header);
+
+                // convert the uploaded file to inputstream
+                InputStream inputStream = inputPart.getBody(InputStream.class, null);
+
+                byte[] bytes = IOUtils.toByteArray(inputStream);
+
+                // constructs upload file path
+                fileName = UPLOADED_FILE_PATH + fileName;
+
+                writeFile(bytes, fileName);
+
+                System.out.println("Done");
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        return Response.status(200).entity("uploadFile is called, Uploaded file name : " + fileName).build();
+
+    }
+
+    /**
+     * header sample { Content-Type=[image/png], Content-Disposition=[form-data;
+     * name="file"; filename="filename.extension"] }
+     **/
+    // get uploaded filename, is there a easy way in RESTEasy?
+    private String getFileName(MultivaluedMap<String, String> header) {
+
+        String[] contentDisposition = header.getFirst("Content-Disposition").split(";");
+
+        for (String filename : contentDisposition) {
+            if ((filename.trim().startsWith("filename"))) {
+
+                String[] name = filename.split("=");
+
+                String finalFileName = name[1].trim().replaceAll("\"", "");
+                return finalFileName;
+            }
+        }
+        return "unknown";
+    }
+
+    //save to somewhere
+    private void writeFile(byte[] content, String filename) throws IOException {
+
+        File file = new File(filename);
+
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+
+        FileOutputStream fop = new FileOutputStream(file);
+
+        fop.write(content);
+        fop.flush();
+        fop.close();
+
+    }
+}
